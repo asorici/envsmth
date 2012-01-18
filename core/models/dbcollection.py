7f4d7707fad2b5c7492cc80cfe0e030ed448a263
Andrei Ciortea <andrei.ciortea@gmail.com>
2012-01-18 16:35:06 +0200

    dbobject update

---
+++ core/models/dbcollection.py
@@ -0,0 +1,36 @@
+class DBCollection(object):
+    
+    """
+    Build collection as a set.
+    """
+    def __init__(self, *obj):
+        if isinstance(obj, list):
+            self.objSet = set(obj)
+        else:
+            self.objSet = set([obj])
+    
+    def addObj(self, obj):
+        self.objSet.add(obj)
+    
+    def removeObj(self, obj):
+        self.objSet.remove(obj)
+    
+    def filter(self, predicate_func):
+        return filter(predicate_func, self.objSet)
+    
+    def save(self):
+        # TODO: batch operation
+        pass
+    
+    def delete(self):
+        # TODO: batch operation 
+        pass
+    
+    @staticmethod
+    def getCollection(domain, **predicate_dict):
+        pass
+    
+    @staticmethod
+    def getCollection(domain, *QObj, **predicate_dict):
+        pass
+    
