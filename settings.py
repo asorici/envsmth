3d2b9d8c704c1d2ff83698ff8e599bba47205c49
Andrei Ciortea <andrei.ciortea@gmail.com>
2012-01-25 16:03:44 +0200

    TagList update

--- settings.py
+++ settings.py
@@ -16,7 +16,7 @@ PROJECT_PATH=os.path.abspath(os.path.dirname(__file__))
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'django',	# Or path to database file if using sqlite3.
+        'NAME': 'envsocial',	# Or path to database file if using sqlite3.
         'USER': 'root',                      # Not used with sqlite3.
         'PASSWORD': 'GroundAce55',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
