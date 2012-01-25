4ea89bf46b8743168b7df837842332371a9e4e6a
Alex Sorici <alex.sorici@gmail.com>
2012-01-25 18:45:21 +0200

    added DateTimeListField and AreaShapeField

--- coresql/db/fields.py
+++ coresql/db/fields.py
@@ -1,16 +1,18 @@
 import string
+from datetime import datetime
 from django.db import models
-from coresql.db.objects import TagList, Data
+from coresql.db.objects import TagList, Data, AreaShape, DateTimeList
 
-class TagField(models.TextField):
+class TagListField(models.TextField):
     
     description = "A semicolon separated list of tags stored as a string"
     
     __metaclass__ = models.SubfieldBase
     
-    def __init__(self, separator = ";", *args, **kwargs):
-        self.separator = separator
-        super(TagField, self).__init__(*args, **kwargs)
+    separator = ";"
+    
+    def __init__(self, *args, **kwargs):
+        super(TagListField, self).__init__(*args, **kwargs)
         
     def to_python(self, value):
         # the object case
@@ -18,12 +20,12 @@ class TagField(models.TextField):
             return value
         
         # the string case which also matches the database case since we subclass TextField
-        tag_list = string.split(value, TagField.separator)
+        tag_list = string.split(value, TagListField.separator)
         return TagList(tags = tag_list)
     
     def get_prep_value(self, value):
         # value is an object of type TagList
-        return self.separator.join(value.getList())
+        return TagListField.separator.join(value.getList())
     
     """
     def get_prep_lookup(self, lookup_type, value):
@@ -37,6 +39,29 @@ class TagField(models.TextField):
     """
 
 
+
+class DateTimeListField(models.TextField):
+    
+    description = "A semicolon separated list of datetime objects stored as a string"
+    
+    __metaclass__ = models.SubfieldBase
+    
+    separator = ";"
+    
+    def to_python(self, value):
+        # the object case
+        if isinstance(value, DateTimeList):
+            return value
+        
+        # the string case which also matches the database case since we subclass TextField
+        datetime_str_list = string.split(value, DateTimeListField.separator)
+        trigger_list = map(lambda d: datetime.strptime(d, '%Y-%m-%d %H:%M:%S'), datetime_str_list)
+        
+        return DateTimeList(triggers = trigger_list)
+    
+    def get_prep_value(self, value):
+        # value is an object of type DateTimeList
+        return DateTimeListField.separator.join( map(lambda d: d.strftime("%Y-%m-%d %H:%M:%S"), value.getList()) )
 
  
 class DataField(models.TextField):
@@ -76,13 +101,13 @@ class AreaShapeField(models.TextField):
         
     def to_python(self, value):
         # the object case
-        if isinstance(value, Data):
+        if isinstance(value, AreaShape):
             return value
         
         # the string case which also matches the database case since we subclass TextField
-        return Data.dbDecode(value, self.encode_func)
+        return AreaShape.dbDecode(value, self.encode_func)
     
     def get_prep_value(self, value):
-        # value is an object of type TagList
+        # value is an object of type AreaShape
         return value.dbEncode()
-     
+    
