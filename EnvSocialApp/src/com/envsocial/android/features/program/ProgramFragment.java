fe8a006b94067865c0427a397894d7afd1a1cb45
Andrei Ciortea <andrei.ciortea@gmail.com>
2012-06-04 15:11:10 +0300

    Program updates

--- EnvSocialApp/src/com/envsocial/android/features/program/ProgramFragment.java
+++ EnvSocialApp/src/com/envsocial/android/features/program/ProgramFragment.java
@@ -1,7 +1,26 @@
 package com.envsocial.android.features.program;
 
+import com.envsocial.android.R;
+
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 public class ProgramFragment extends Fragment {
 	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	}
+	
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+							Bundle savedInstanceState) {
+		// Inflate layout for this fragment.
+		View v = inflater.inflate(R.layout.program, container, false);
+		
+		return v;
+	}
 }
