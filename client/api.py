c1c5dadeb75a1612612e53d3b9238642757d3756
Alex Sorici <alex.sorici@gmail.com>
2013-01-16 15:34:11 +0200

    Client side Location class is now aware of admin field; Features retrieval now only serializes environment or area fields but not both (since a feature is attached to only one location type)

--- client/api.py
+++ client/api.py
@@ -202,7 +202,6 @@ class EnvironmentResource(ModelResource):
                 ## attach resource_uri and environment_uri
                 feat_dict['resource_uri'] = FeatureResource().get_resource_uri(feature)
                 feat_dict['environment'] = self.get_resource_uri(bundle)
-                feat_dict['area'] = None
                 feature_list.append(feat_dict)
                 
         return feature_list
@@ -298,7 +297,6 @@ class AreaResource(ModelResource):
                 ## attach resource_uri and area_uri
                 feat_dict['resource_uri'] = FeatureResource().get_resource_uri(feature)
                 feat_dict['area'] = self.get_resource_uri(bundle)
-                feat_dict['environment'] = None
                 feature_list.append(feat_dict)
         
         ## then see if environment features which also apply to the area are available - e.g. program, order
@@ -310,7 +308,6 @@ class AreaResource(ModelResource):
             if feat_dict:
                 ## attach resource_uri and area_uri
                 feat_dict['resource_uri'] = FeatureResource().get_resource_uri(env_feat)
-                feat_dict['area'] = None
                 feat_dict['environment'] = EnvironmentResource().get_resource_uri(environment)
                 feature_list.append(feat_dict)
         
@@ -406,6 +403,16 @@ class FeatureResource(ModelResource):
         filters = bundle.request.GET.copy()
         return bundle.obj.get_feature_data(filters)
     
+    
+    def dehydrate(self, bundle):
+        if bundle.obj.environment is None:
+            del bundle.data['environment']
+        
+        elif bundle.obj.area is None:
+            del bundle.data['area']
+    
+        return bundle
+
 
 class AnnouncementResource(ModelResource):
     environment = fields.ForeignKey(EnvironmentResource, 'environment')
