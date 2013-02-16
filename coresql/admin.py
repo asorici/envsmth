0ef694a0afbeed0d230f0ee96b9b494d1b8fcc13
Alex Sorici <alex.sorici@gmail.com>
2013-02-16 21:46:26 +0200

    Adding SocialMediaFeature model to django admin

--- coresql/admin.py
+++ coresql/admin.py
@@ -2,11 +2,11 @@ from django.contrib import admin
 from coresql.models import UserProfile, Environment, Layout, Area, Announcement,\
     Annotation, ProgramAnnotation, History, Privacy,\
     Feature, DescriptionFeature, BoothDescriptionFeature, BoothProduct,\
-    ProgramFeature, Presentation, Session, Speaker,\
+    ProgramFeature, Presentation, Session, Speaker, SocialMediaFeature,\
     MenuCategory, MenuItem, UserSubProfile, ResearchProfile
 
 admin.site.register([UserProfile, Environment, Layout, 
                      Area, Announcement, Annotation, ProgramAnnotation, History, 
                      Privacy, Feature, DescriptionFeature, BoothDescriptionFeature, BoothProduct, 
-                     ProgramFeature, Presentation, Session, Speaker, 
+                     ProgramFeature, Presentation, Session, Speaker, SocialMediaFeature,
                      MenuCategory, MenuItem, UserSubProfile, ResearchProfile])
